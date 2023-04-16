a7710a8086857208eef80bb0b0e41bd46907f45e
EduCostam <eduardomc.devs@gmail.com>
2023-04-16 20:22:19 -0300

    const

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const request = require("request-promise-native");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const url = "https://tappedout.net/mtg-decks/primal-rain-2/?cb=1681526968";
