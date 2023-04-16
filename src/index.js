b088b5cbdefebaac67e4d5a0309889252ad0c51b
EduCostam <eduardomc.devs@gmail.com>
2023-04-16 20:30:30 -0300

    const JSON.stringify

--- src/index.js
+++ src/index.js
@@ -11,14 +11,20 @@ const url = "https://tappedout.net/mtg-decks/primal-rain-2/?cb=1681526968";
 
     const cards = [];
 
-    $('div.boardList table tbody tr').each((i, element) => {
-      const name = $(element).find('td.card a').text().trim();
-      const amount = $(element).find('td.amount').text().trim();
-      const edition = $(element).find('td.language').text().trim();
+    $("div.boardList table tbody tr").each((i, element) => {
+      const name = $(element).find("td.card a").text().trim();
+      const amount = $(element).find("td.amount").text().trim();
+      const edition = $(element).find("td.language").text().trim();
 
-      if (language === 'English') {
+      if (language === "English") {
         cards.push({ name, amount, edition });
       }
-    })
+    });
+
+    const json = JSON.stringify({ cards }, null, 2);
+    fs.writeFileSync("cards.json", json);
+    console.log("Cards successfuly saved to cards.json");
+  } catch (err) {
+    console.log(err);
   }
-})
+})();
